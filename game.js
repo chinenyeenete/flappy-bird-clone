a8c34ad2ad517bdb777522137e8f034d9285a745
heyyitscandee <189556569+IamChinenye@users.noreply.github.com>
2025-07-08 17:51:40 -0400

    added sprite

--- game.js
+++ game.js
@@ -23,11 +23,34 @@ function preload(){
   this.load.image('column', 'assets/column.png');
   this.load.spritesheet('bird', 'assets/bird.png', { frameWidth: 64, frameHeight: 96 });
 }
+
+let bird;
+let hasLanded = false;
+
 function create(){
     const background = this.add.image(0, 0, 'background').setOrigin(0, 0);
     background.displayWidth = this.sys.game.config.width;
     background.displayHeight = this.sys.game.config.height;
+    const roads = this.physics.add.staticGroup();
+    const topCols = this.physics.add.staticGroup({
+        key: "column",
+        repeat: 7,
+        setXY: {x:200, y: 0.25*innerHeight, stepX: 300},
+    });
+    const bottomCols = this.physics.add.staticGroup({
+        key: "column",
+        repeat: 7,
+        setXY: {x: 350, y: 0.68*innerHeight, stepX: 300}
+    });
+    
+    const road = roads.create(background.displayWidth/2, background.displayHeight, "road").setScale(2).refreshBody();
+    road.displayWidth = background.displayWidth;
+
+    bird = this.physics.add.sprite(0, 50, "bird").setScale(2)
+    bird.setBounce(0.2);
+    bird.setCollideWorldBounds(true);
 }
+
 function update(){
 
 }
